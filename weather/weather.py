2afa992fec022b346a849df559031d9e0e428d8a
Brian Hines <brian@projectweekend.net>
2015-06-26 18:19:15 -0500

    Whoops

--- weather/weather.py
+++ weather/weather.py
@@ -6,6 +6,5 @@ FORECAST = forecastio.load_forecast(FORECAST_IO_KEY, LATITUDE, LONGITUDE)
 
 
 def weather_info():
-    # currently = FORECAST.currently()
-    # return currently.d
-    return 'Weather'
+    currently = FORECAST.currently()
+    return currently.d
