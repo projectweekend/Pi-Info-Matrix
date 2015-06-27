b84ddd518517868ff9f580c3720504a7a59dcc8a
Brian Hines <brian@projectweekend.net>
2015-06-26 23:10:06 -0500

    Write to matrix

--- info_matrix/info_matrix.py
+++ info_matrix/info_matrix.py
@@ -32,3 +32,5 @@ class InfoMatrix(object):
     def write_bus(self, route, predictions):
         image, draw = self._image_draw()
         draw.text((0, 0), '{0}:'.format(route), (255, 0, 0), font=self._font)
+        self._matrix.Clear()
+        self._matrix.SetImage(image.im.id, 0, 0)
